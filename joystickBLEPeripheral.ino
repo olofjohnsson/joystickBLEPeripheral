eec348fb3e1f683f80891934d34fe4c95270bc18
olofjohnsson <olof.s.johnsson@gmail.com>
2022-10-08 08:42:34 +0200

    Initial working version

--- joystickBLEPeripheral.ino
+++ joystickBLEPeripheral.ino
@@ -3,7 +3,7 @@
 */
 
 #include <ArduinoBLE.h>
-#define UPDATE_THRESH 2
+#define UPDATE_THRESH 15
 #define UPDATE_INTERVAL 10
 
 #define UUID_Service 76ad7aaa-3782-11ed-a261-0242ac120002
@@ -19,8 +19,8 @@ bool stateChanged = false;
 bool button_2_State = false;  
 bool button_3_State = false; 
 bool button_4_State = false; 
-bool turningDirection = false; //Default direction is left
-bool runningDirection = false;//Default is forward
+bool turningDirection = 0; //Default direction is left
+bool runningDirection = 0;//Default is forward
 
 int x_readingRaw = 0;
 int y_readingRaw = 0;
@@ -37,11 +37,13 @@ BLEUnsignedCharCharacteristic batteryLevelChar("2A19",  // standard 16-bit chara
 
 BLEUnsignedIntCharacteristic x_readingChar("76ad7aa1-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
 BLEUnsignedIntCharacteristic y_readingChar("76ad7aa2-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
+BLEUnsignedIntCharacteristic x_rawReadingChar("76ad7ab1-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
+BLEUnsignedIntCharacteristic y_rawReadingChar("76ad7ab2-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
 BLEBooleanCharacteristic button2Char("76ad7aa3-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
 BLEBooleanCharacteristic button3Char("76ad7aa4-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
 BLEBooleanCharacteristic button4Char("76ad7aa5-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
-BLEBooleanCharacteristic turningDirectionChar("76ad7aa6-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
-BLEBooleanCharacteristic runningDirectionChar("76ad7aa7-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
+BLEUnsignedIntCharacteristic turningDirectionChar("76ad7aa6-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
+BLEUnsignedIntCharacteristic runningDirectionChar("76ad7aa7-3782-11ed-a261-0242ac120002", BLERead | BLENotify);
 
 int x_prevReading = 0;  // last battery level reading from analog input
 int y_prevReading = 0;  // last battery level reading from analog input
@@ -80,6 +82,8 @@ void setup() {
   BLE.setAdvertisedService(joystickService); // add the service UUID
   joystickService.addCharacteristic(x_readingChar); // add the x reading characteristic
   joystickService.addCharacteristic(y_readingChar); // add the y reading characteristic
+  joystickService.addCharacteristic(x_rawReadingChar); // add the x reading characteristic
+  joystickService.addCharacteristic(y_rawReadingChar); // add the y reading characteristic
   joystickService.addCharacteristic(button2Char);
   joystickService.addCharacteristic(button3Char);
   joystickService.addCharacteristic(button4Char);
@@ -157,26 +161,38 @@ void updateAnalogReading() {
   */
   x_readingRaw = analogRead(A0);
   y_readingRaw = analogRead(A2);
-  if (x_readingRaw < 512)
+  if (x_readingRaw < 490)
   {
-    x_reading = map(x_readingRaw, 0, 511, 0, 255);
-    turningDirection = false;
+    x_reading = map(x_readingRaw, 0, 511, 255, 0);
+    if (x_reading > 15)
+    {  
+      turningDirection = 0;
+    }
   }
-  if (x_readingRaw > 511)
+  if (x_readingRaw > 530)
   {
     x_reading = map(x_readingRaw, 512, 1024, 0, 255);
-    turningDirection = true;
+    if (x_reading > 15)
+    {  
+      turningDirection = 1;
+    }
   }
 
-  if (y_readingRaw < 512)
+  if (y_readingRaw < 490)
   {
-    y_reading = map(x_readingRaw, 0, 511, 0, 255);
-    runningDirection = false;
+    y_reading = map(y_readingRaw, 0, 511, 255, 0);
+    if (y_reading > 15)
+    {  
+      runningDirection = 0;
+    }
   }
-  if (y_readingRaw > 511)
+  if (y_readingRaw > 530)
   {
-    y_reading = map(x_readingRaw, 512, 1024, 0, 255);
-    runningDirection = true;
+    y_reading = map(y_readingRaw, 512, 1024, 0, 255);
+    if (y_reading > 15)
+    {  
+      runningDirection = 1;
+    }
   }
 
   if (abs(x_reading-x_prevReading)>UPDATE_THRESH) // if the analog reading level has changed beyond preset threshold
@@ -184,9 +200,9 @@ void updateAnalogReading() {
       Serial.print("x_reading: "); // print it
       Serial.println(x_reading);
       Serial.print("turningDirection: ");
-      Serial.println(turningDirection);
+      Serial.println((byte)turningDirection);
       x_readingChar.writeValue((byte)x_reading);  // and update the characteristic
-      turningDirectionChar.writeValue(turningDirection);
+      turningDirectionChar.writeValue((byte)turningDirection);
       x_prevReading = x_reading;           // save the level for next comparison
     }
   
@@ -195,11 +211,13 @@ void updateAnalogReading() {
     Serial.print("y_reading: "); // print it
     Serial.println(y_reading);
     Serial.print("runningDirection: ");
-    Serial.println(runningDirection);
+    Serial.println((byte)runningDirection);
     y_readingChar.writeValue((byte)y_reading); // Update characteristic
-    runningDirectionChar.writeValue(turningDirection);
+    runningDirectionChar.writeValue((byte)runningDirection);
     y_prevReading = y_reading;           // save the level for next comparison
   }
+  x_reading = 0;
+  y_reading = 0;
 }
 
 void loop() {
